063956786409b23593fc1e85ce49c87490792f9b
Hamid <>
2022-05-23 15:18:45 +0430

    cart and local storage added to project

--- js/app.js
+++ js/app.js
@@ -1,36 +1,76 @@
 let count = 0;
 let sum = 0;
+let cart = {};
 
-let cart = [];
+if (localStorage.getItem("count")) {
+    count = parseInt(localStorage.getItem("count"));
+}
+
+if (localStorage.getItem("sum")) {
+    sum = parseInt(localStorage.getItem("sum"));
+}
+
+if (localStorage.getItem("cart")) {
+    cart = JSON.parse(localStorage.getItem("cart"));
+}
+
+updateCart();
 
 let btns = document.querySelectorAll(".products button");
 
 for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", add);
+    let btn = btns[i];
+    btn.addEventListener("click", add);
+
+    // id = btn.dataset.id;
+    // if (cart.indexOf(id) >= 0) {
+    //     btn.className = "added";
+    //     btn.textContent = "Remove";
+    // }
 }
 
 function add(event) {
     let price = Number(event.target.dataset.price);
+    let title = event.target.dataset.title;
+    let id = event.target.dataset.id;
 
-    let index = cart.indexOf(event.target.dataset.id);
-    console.log(cart, index);
-    if (index >= 0) {
-        cart.splice(index, 1)
-        count--;
-        sum -= price;
-        event.target.className = "";
-        event.target.textContent = "Add to cart";
+    if (id in cart) {
+        cart[id].qty++;
     } else {
-        cart.push(event.target.dataset.id);
-        count++;
-        sum += price;
-        event.target.className = "added";
-        event.target.textContent = "Remove";
+        let cartItem = {
+            title: title,
+            price: price,
+            qty: 1
+        };
+        cart[id] = cartItem
     }
+
+    count++;
+    sum += price;
+
+    console.log(cart);
+
+    // let index = cart.indexOf(event.target.dataset.id);
+    // if (index >= 0) {
+    //     cart.splice(index, 1);
+    //     count--;
+    //     sum -= price;
+    //     event.target.className = "";
+    //     event.target.textContent = "Add to cart";
+    // } else {
+    //     cart.push(event.target.dataset.id);
+    //     count++;
+    //     sum += price;
+    //     event.target.className = "added";
+    //     event.target.textContent = "Remove";
+    // }
+    localStorage.setItem("cart", JSON.stringify(cart));
     updateCart();
 }
 
 function updateCart() {
     document.getElementById("sum").textContent = sum;
     document.getElementById("count").textContent = count;
+    localStorage.setItem("sum", sum);
+    localStorage.setItem("count", count);
 }
---
+++ js/cart.js
@@ -0,0 +1,28 @@
+let cart = {};
+if (localStorage.getItem("cart")) {
+    cart = JSON.parse(localStorage.getItem("cart"));
+}
+
+let tbody = document.getElementById("tbody");
+
+for (let id in cart) {
+    let item = cart[id];
+
+    let tr = document.createElement('tr')
+
+    let title_td = document.createElement('td')
+    title_td.textContent = item.title
+    tr.appendChild(title_td)
+
+
+    let price_td = document.createElement("td");
+    price_td.textContent = item.price;
+    tr.appendChild(price_td);
+
+    let qty_td = document.createElement("td");
+    qty_td.textContent = item.qty;
+    tr.appendChild(qty_td);
+
+    tbody.appendChild(tr)
+
+}
